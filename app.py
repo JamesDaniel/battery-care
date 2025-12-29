c7eb84899cd00ad9a2d83f2ad274135e6bdd5bd9
JamesDaniel <jamesmcgarr@protonmail.com>
2025-12-29 22:25:37 +0000

    Fixed environment .env setup.

--- app.py
+++ app.py
@@ -4,18 +4,27 @@ from tapo import ApiClient
 import time
 import psutil
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 async def main():
 
 
     username = os.environ.get("API_USERNAME")
     password = os.environ.get("API_PASSWORD")
+    device_ip = os.environ.get("DEVICE_IP_ADDRESS")
+
+    if not username or not password or not device_ip:
+        print("API_USERNAME, API_PASSWORD, and DEVICE_IP_ADDRESS must be set in the .env file.")
+        return
 
     while True:
         print('start of loop')
         time.sleep(30)
         client = ApiClient(username, password)
-        device = await client.p110("192.168.0.11")
+        device = await client.p110(device_ip)
         device_info = await device.get_device_info()
         device_on = device_info.device_on
         battery = psutil.sensors_battery()
